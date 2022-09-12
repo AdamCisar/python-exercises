54525d6b6b6d10264235772bc150cb59302c602a
Adam Čisár <adamcisar7@gmail.com>
2022-09-12 19:35:37 +0200

    ticha aukcia

---
+++ silence_auction.py
@@ -0,0 +1,37 @@
+from Logo_auction import auction_logo
+import os
+print(auction_logo)
+print('Vitajte v programu pre tichu drazbu')
+
+buyers = {
+
+}
+highest_offer = []
+
+while True:
+    
+    name = input('Ake je vase meno\n')
+    offer = int(input('Aka je vasa ponuka?\n'))
+    #pridavanie do slovnika
+    buyers[name] = offer
+    
+    #zastavenie alebo pokracovanie
+    decision = input('Su dalsi kupujuci? Napiste "ano" alebo "nie"\n')
+        
+    if decision == 'nie':
+        break
+    
+os.system('cls||clear')
+
+#najvacsia ponuka
+number = 0
+for key in buyers:
+    if number < buyers[key]:
+        number = buyers[key]
+
+
+#hladanie vitaza
+for winner in buyers:
+    if number == buyers[winner]:
+        print(f'Vitazom je {winner} s ponukou {number}')
+
